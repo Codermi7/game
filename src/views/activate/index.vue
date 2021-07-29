deff26658db1f0fdf2528268f94771c343eacceb
15576697952 <724316662@qq.com>
2021-07-29 14:55:27 +0800

    first commit

---
+++ src/views/activate/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <big-wheel></big-wheel>
+</template>
+
+<script>
+    import {BigWheel} from "../../components";
+
+    export default {
+        name: "index",
+        components: {
+            BigWheel
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/home/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <drawer-layout :drawerWidth="300" :triggerArea="0.2" btnId="enum">
+      <div slot="drawer" class="slider">
+        Login
+        <h2 @click="$router.replace('/login')">登录</h2>
+      </div>
+      <div slot="content" class="main">
+        <div id="enum">菜单</div>
+        this is Context
+      </div>
+    </drawer-layout>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import {DrawerLayout} from "../components";
+
+export default {
+  name: 'Home',
+  components:{
+    DrawerLayout
+  }
+}
+</script>
+<style lang="less" scoped>
+  .main {
+    background: #fff;
+    height: 100vh;
+  }
+  .slider {
+    background: #42b983;
+    height: 100vh;
+  }
+</style>
